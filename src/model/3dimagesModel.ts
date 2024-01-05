ad12f0a752b6fbe36d1a41849feb0d36a1c721ec
Saumya Srivastava <104627870+SAUMYXA@users.noreply.github.com>
2024-01-05 15:12:49 +0530

    Update 3dimagesModel.ts

--- src/model/3dimagesModel.ts
+++ src/model/3dimagesModel.ts
@@ -4,7 +4,5 @@ const mongoose = require('mongoose');
 const adminfor3DSchema = new mongoose.Schema({
   apparel: String, // Add a field to store the section tag
   photo1: String, // URL for the first image
-  
 });
-
 module.exports = mongoose.model('images3D', adminfor3DSchema);
